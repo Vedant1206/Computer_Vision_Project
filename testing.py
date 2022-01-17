b74e279d59c5fcaa64606768f8ee620226fd844a
Vedant1206 <70309263+Vedant1206@users.noreply.github.com>
2022-01-16 20:21:10 -0600

    all these methods are made for fun and testing github no significance to project

--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
+#all these methods are made for fun and testing github no significance to project
 class x:
     def __init__(self, name):
         self.name = name
 
-
+class Stack:
+    def __init__(self, name):
+        self.name = name
